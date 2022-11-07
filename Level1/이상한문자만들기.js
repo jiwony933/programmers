3d2c426f4dbd49dc5c80a3d23e8bfe8020637149
Jiwon <jiwony933@naver.com>
2022-11-07 14:17:24 +0900

    lv1 이상한문자만들기

---
+++ Level1/이상한문자만들기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    var answer = [];
+    let words = s.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        let changed = "";
+        for (let j = 0; j < words[i].length; j++) {
+            changed +=
+                j % 2 !== 1
+                    ? words[i][j].toUpperCase()
+                    : words[i][j].toLowerCase();
+        }
+
+        answer.push(changed);
+    }
+    return answer.join(" ");
+}
