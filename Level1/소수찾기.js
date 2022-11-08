2df1074d71c6673c1d0cab59467a1f66db916605
Jiwon <jiwony933@naver.com>
2022-11-08 10:28:45 +0900

    lv1 소수찾기 : 시간초과로 통과실패

---
+++ Level1/소수찾기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = 1;
+    for (i = 3; i <= s; i++) {
+        let temp = 0;
+        if (i % 2 === 0) continue;
+        for (j = 1; j <= i / 2; j++) {
+            i % j === 0 && temp++;
+        }
+        temp === 1 && answer++;
+    }
+    return answer;
+}
+
+console.log(solution(10));
