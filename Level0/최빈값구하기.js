e2c99af792faa208cc2c3eb54e5b4ca44d796fd6
Jiwon <jiwony933@naver.com>
2022-11-11 15:31:12 +0900

    level 0 폴더 생성

---
+++ Level0/최빈값구하기.js
@@ -0,0 +1,6 @@
+function solution(array) {
+  var answer = 0;
+  const filtered = new Set(array);
+
+  return answer;
+}
--- Level1/이상한문자만들기.js
+++ Level1/이상한문자만들기.js
@@ -1,17 +1,15 @@
 function solution(s) {
-    var answer = [];
-    let words = s.split(" ");
+  var answer = [];
+  let words = s.split(" ");
 
-    for (let i = 0; i < words.length; i++) {
-        let changed = "";
-        for (let j = 0; j < words[i].length; j++) {
-            changed +=
-                j % 2 !== 1
-                    ? words[i][j].toUpperCase()
-                    : words[i][j].toLowerCase();
-        }
-
-        answer.push(changed);
+  for (let i = 0; i < words.length; i++) {
+    let changed = "";
+    for (let j = 0; j < words[i].length; j++) {
+      changed +=
+        j % 2 !== 1 ? words[i][j].toUpperCase() : words[i][j].toLowerCase();
     }
-    return answer.join(" ");
+
+    answer.push(changed);
+  }
+  return answer.join(" ");
 }
